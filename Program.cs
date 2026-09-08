1677b0b6e026d6bfb45faebdfebfdea256644286
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-08 15:50:39 -0700

    Mini Challenge 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Chris Estrada
+//Mini Challenge One Say Hello
+//I used a variable to save a user's name
+//Peer Review name:
+//Review:
+
+Console.WriteLine("What is your name?");
+string myName = Console.ReadLine();
+Console.WriteLine("Hello " + myName);
